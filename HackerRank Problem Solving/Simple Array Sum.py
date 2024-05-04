6c8edcc1e3c1b1ba245095916c8fd7753ede5d4c
Pranav Anand <pranavanand2410@gmail.com>
2024-05-04 22:25:50 +0530

    Deleted HackerRank Problem Solving directory

--- HackerRank Problem Solving/Simple Array Sum.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-def simpleArraySum(ar):
-    sum = 0;
-    for i in range(len(ar)):
-        sum += ar[i]
-    return (sum)
--- HackerRank Problem Solving/Staircase.py
+++
@@ -1,9 +0,0 @@
-import math
-import os
-import random
-import re
-import sys
-
-def staircase(n):
-    for i  in range(1, n+1):
-        print(' ' * (n - i) + '#' * i)
