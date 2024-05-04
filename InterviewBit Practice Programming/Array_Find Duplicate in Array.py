22234bd85d98107264850c4194f82810b1420d4b
Pranav Anand <pranavanand2410@gmail.com>
2024-05-04 22:25:15 +0530

    Deleted InterviewBit Practice Programming directory

--- InterviewBit Practice Programming/Array_Find Duplicate in Array.py
+++
@@ -1,5 +0,0 @@
-def repeatedNumber(self, A):
-    sumOfList = sum(A)
-    n = len(A)
-    sumOfRange = int(n * (n + 1) / 2) - n
-    return sumOfList - sumOfRange
--- InterviewBit Practice Programming/Math_Palindrome Integer.py
+++
@@ -1,3 +0,0 @@
-class solution:
-    def isPalindrome(self,A):
-        return 1 if str(A) == str(A)[::-1] else 0
